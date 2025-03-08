1ca70a9cfa125277b72c41d640ac5232709d2198
terrysandybo <2113865017@qq.com>
2025-03-09 00:04:08 +0800

    Add basic React project structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Sandy Project</h1>
+        <p>Your new website is ready!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
